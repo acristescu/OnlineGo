4536e0c8408413af9e35636758e94d1a0d7590c1
Alex <alexandru.cristescu@farepilot.com>
2018-05-05 18:45:32 +0100

    Fixed crash when game fragment was reinstantiated

--- app/src/main/java/io/zenandroid/onlinego/game/GameFragment.kt
+++ app/src/main/java/io/zenandroid/onlinego/game/GameFragment.kt
@@ -105,6 +105,11 @@ class GameFragment : Fragment(), GameContract.View {
             board.boardSize = value
         }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_game, container, false)
         unbinder = ButterKnife.bind(this, view)
