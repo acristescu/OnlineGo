d616221e094dc7a0785a1aacb152542b7746dde2
Alexandru Cristescu <acristescu@gmail.com>
2021-08-13 17:57:32 +0100

    More tutorials work

--- app/src/main/java/io/zenandroid/onlinego/ui/screens/mygames/MyGamesFragment.kt
+++ app/src/main/java/io/zenandroid/onlinego/ui/screens/mygames/MyGamesFragment.kt
@@ -9,6 +9,29 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.SimpleItemAnimator
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnLifecycleDestroyed
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import com.awesomedialog.blennersilva.awesomedialoglibrary.AwesomeInfoDialog
@@ -24,6 +47,9 @@ import io.zenandroid.onlinego.ui.items.*
 import io.zenandroid.onlinego.ui.screens.game.GAME_ID
 import io.zenandroid.onlinego.ui.screens.game.GAME_SIZE
 import io.zenandroid.onlinego.ui.screens.whatsnew.WhatsNewDialog
+import io.zenandroid.onlinego.ui.theme.OnlineGoTheme
+import io.zenandroid.onlinego.ui.theme.salmon
+import io.zenandroid.onlinego.ui.theme.shapes
 import io.zenandroid.onlinego.utils.showIf
 import org.koin.android.ext.android.get
 
@@ -52,6 +78,16 @@ class MyGamesFragment : Fragment(), MyGamesContract.View {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentMygamesBinding.inflate(inflater, container, false)
+        binding.tutorialView.apply {
+            setViewCompositionStrategy(
+                DisposeOnLifecycleDestroyed(viewLifecycleOwner)
+            )
+            setContent {
+                OnlineGoTheme {
+                    TutorialItem(percentage = 73, tutorial = "Advanced > Snapback")
+                }
+            }
+        }
         return binding.root
     }
 
@@ -169,4 +205,81 @@ class MyGamesFragment : Fragment(), MyGamesContract.View {
 
     override fun isHistoricGamesSectionEmpty() =
         groupAdapter.olderGamesAdapter.isEmpty()
+}
+
+
+@Composable
+fun TutorialItem(percentage: Int, tutorial: String) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "$percentage %",
+            fontWeight = FontWeight.Black,
+            color = salmon,
+            modifier = Modifier
+                .align(CenterVertically)
+                .padding(24.dp)
+        )
+        Surface(
+            color = salmon,
+            shape = RoundedCornerShape(topStart = 20.dp, bottomStart = 20.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(90.dp)
+                .padding(start = 12.dp)
+        ) {
+            Row {
+                Canvas(
+                    modifier = Modifier
+                        .size(25.dp, 50.dp)
+                        .align(CenterVertically)) {
+                    drawArc(
+                        color = Color.White,
+                        alpha = .25f,
+                        startAngle = 90f,
+                        sweepAngle = -180f,
+                        useCenter = false,
+                        topLeft = Offset(-size.width, 0f),
+                        size = Size(size.width * 2, size.height),
+                        style = Stroke(width = 24.dp.value)
+                    )
+                    drawArc(
+                        color = Color.White,
+                        startAngle = 90f,
+                        sweepAngle = -180f * .73f,
+                        useCenter = false,
+                        topLeft = Offset(-size.width, 0f),
+                        size = Size(size.width * 2, size.height),
+                        style = Stroke(
+                            width = 24.dp.value,
+                            cap = StrokeCap.Round
+                        )
+                    )
+                }
+                Column {
+                    Text(
+                        text = "Learn to play",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 14.sp,
+                        modifier = Modifier.padding(start = 70.dp, top = 20.dp)
+                    )
+                    Text(
+                        text = tutorial,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 12.sp,
+                        modifier = Modifier.padding(start = 70.dp, top = 18.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Preview() {
+    OnlineGoTheme {
+        TutorialItem(percentage = 73, tutorial = "Basics > Capturing")
+    }
 }
