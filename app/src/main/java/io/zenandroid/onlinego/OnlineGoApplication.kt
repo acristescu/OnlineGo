d80f76beb5bb752eea50eb98339e4b8925eb5257
Alexandru Cristescu <acristescu@gmail.com>
2025-09-27 11:08:06 +0400

    Refactor: Enable network stack pre-warming
    This commit re-enables the pre-warming of the network stack in `OnlineGoApplication.kt`.

--- app/src/main/java/io/zenandroid/onlinego/OnlineGoApplication.kt
+++ app/src/main/java/io/zenandroid/onlinego/OnlineGoApplication.kt
@@ -3,12 +3,18 @@ package io.zenandroid.onlinego
 import android.app.Application
 import android.util.Log
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import io.reactivex.exceptions.UndeliverableException
 import io.reactivex.plugins.RxJavaPlugins
+import io.zenandroid.onlinego.data.ogs.OGSRestService
+import io.zenandroid.onlinego.data.ogs.OGSWebSocketService
 import io.zenandroid.onlinego.di.allKoinModules
+import io.zenandroid.onlinego.gamelogic.RulesManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -39,19 +45,19 @@ class OnlineGoApplication : Application() {
 
             modules(allKoinModules)
         }
-//        applicationScope.launch(Dispatchers.IO) {
-//            Log.d("AppInit", "Pre-warming network stack on ${Thread.currentThread().name}")
-//            try {
-//                // Eagerly resolve the dependencies that are slow
-//                getKoin().get<OGSRestService>()
-//                getKoin().get<OGSWebSocketService>()
-//                Log.d("AppInit", "Network stack pre-warmed")
-//            } catch (e: Exception) {
-//                Log.e("AppInit", "Error pre-warming Koin dependencies", e)
-//            }
-//            RulesManager.coordinateToCell("A1")
-//            FirebaseCrashlytics.getInstance().log("Done pre-warming network stack")
-//        }
+        applicationScope.launch(Dispatchers.IO) {
+            Log.d("AppInit", "Pre-warming network stack on ${Thread.currentThread().name}")
+            try {
+                // Eagerly resolve the dependencies that are slow
+                getKoin().get<OGSRestService>()
+                getKoin().get<OGSWebSocketService>()
+                Log.d("AppInit", "Network stack pre-warmed")
+            } catch (e: Exception) {
+                Log.e("AppInit", "Error pre-warming Koin dependencies", e)
+            }
+            RulesManager.coordinateToCell("A1")
+            FirebaseCrashlytics.getInstance().log("Done pre-warming network stack")
+        }
 
         RxJavaPlugins.setErrorHandler {
             val e = if (it is UndeliverableException) it.cause else it
